aea0ff2a852ff8ad08ddfe658e972a30484ef2d4
Adbul Aziz <aziz97cse@gmail.com>
2019-11-16 22:38:59 +0600

    password reset complete

--- app/Http/Controllers/Admin/ForgotPasswordController.php
+++ app/Http/Controllers/Admin/ForgotPasswordController.php
@@ -4,9 +4,15 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Support\Facades\Password;
 
 class ForgotPasswordController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest:admin');
+    }
+
     /*
     |--------------------------------------------------------------------------
     | Password Reset Controller
@@ -19,4 +25,24 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+    /**
+     * Display the form to request a password reset link.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLinkRequestForm()
+    {
+        return view('admin.passwords.email');
+    }
+
+    /**
+     * Get the broker to be used during password reset.
+     *
+     * @return \Illuminate\Contracts\Auth\PasswordBroker
+     */
+    public function broker()
+    {
+        return Password::broker('admins');
+    }
 }
